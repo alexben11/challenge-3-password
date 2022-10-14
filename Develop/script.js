2faddbf99fc23f420bbf87a0db8b0182a4fcf914
alexben11 <113950983+alexben11@users.noreply.github.com>
2022-10-13 21:59:05 -0400

    keep on trying

--- Develop/script.js
+++ Develop/script.js
@@ -6,21 +6,19 @@ var numbers = [0,1,2,3,4,5,6,7,8,9];
 
 var generateBtn = document.querySelector("#generate");
 
-userInput = window.prompt ("How long");
-
 // Write password to the #password input
   function writePassword() {
-    userInput = window.prompt ("How long");
-    var password = generatePassword(){
-      
+    var userInput = window.confirm ("hello");
+    console.log (userInput)
+
+    var password = generatePassword() 
 
     }
     var passwordText = document.querySelector("#password");
-
   
     passwordText.value = password;
 
-  }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
